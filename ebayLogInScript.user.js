de64e2a51e38e0023d249858aecf30d64b058b23
MrJones1234 <47802004+MrJones1234@users.noreply.github.com>
2019-02-20 11:53:36 +0900

    @include updated

--- ebayLogInScript.user.js
+++ ebayLogInScript.user.js
@@ -2,11 +2,12 @@
 // @name     ebayLogInScript
 // @version  1
 // @grant    none
+// @include *
 // ==/UserScript==
 
 
 var getUrlVars = function(){
-  var vars = {}; 
+  var vars = {};
   var param = location.search.substring(1).split('&');
   for(var i = 0; i < param.length; i++) {
     var keySearch = param[i].search(/=/);
@@ -15,27 +16,27 @@ var getUrlVars = function(){
     var val = param[i].slice(param[i].indexOf('=', 0) + 1);
     if(key != '') vars[key] = decodeURI(val);
   }
-  return vars; 
+  return vars;
 }
 var getVal = getUrlVars();
 
-if(  location.hostname == ('www.ebay.co.uk' || 'www.ebay.co.de' || 'www.ebay.com') && getVal.userid && getVal.pass){
+if(location.hostname == ('www.ebay.co.uk' || 'www.ebay.co.de' || 'www.ebay.com') && getVal.userid && getVal.pass){
 
   (function(d, i, v) {
-    
+
     d[i]("userid")[v] = atob(getVal.userid);
     d[i]("pass")[v] = atob(getVal.pass);
     d[i]("SignInForm").submit();
-    
+
   })
-   
-  
+
+
   (document, "getElementById" ,"value");
 	}
 
 
-if(  (location.hostname == 'www.ebay.com' && getVal.redirect) ){
-    
+if((location.hostname == 'www.ebay.com' && getVal.redirect) ){
+
     window.location = "https://" + getVal.redirect;
-    
+
    }
